d76ce95e9d33e1d3d8235e1118d2de40869948b6
nicelifeBS <nicelife.bs@gmail.com>
2016-10-26 20:06:32 +1300

    YetiToolbox with working functionality

---
+++ python/YetiHelpers.py
@@ -0,0 +1,117 @@
+import os
+import logging
+from datetime import datetime
+
+import pymel.core as pm
+
+logger = logging.getLogger('YetiToolbox')
+
+def get_project_dir(_type=None):
+    """
+    Returns the project directory or a file rule path defined as type
+
+    :param _type: fileRule type
+    :type _type: str
+    :return:
+    """
+    ws = pm.workspace
+    if _type:
+        return os.path.join(ws.getPath(), ws.fileRules[_type])
+    else:
+        return ws.getPath()
+
+def get_time_range():
+    start = pm.playbackOptions(ast=True, query=True)
+    end = pm.playbackOptions(aet=True, query=True)
+
+    return start, end
+
+def get_selection():
+    return pm.selected()
+
+def get_yeti_nodes(selection=False):
+    """
+    Return list of yeti nodes in the scene or selection
+    :param selection:
+    :return:
+    """
+    data = []
+    if selection:
+        for s in pm.selected():
+            if s.type() == 'pgYetiMaya':
+                data.append(s)
+                continue
+            try:
+                shape = s.getShape()
+            except AttributeError:
+                continue
+            if shape.type() == 'pgYetiMaya':
+                data.append(shape)
+    else:
+        data = pm.ls(type='pgYetiMaya')
+
+    return data
+
+def set_image_path(path, selection=False):
+    """
+    Set image path of yeti nodes in scene or selection
+    :param path:
+    :param selection:
+    :return:
+    """
+    nodes = get_yeti_nodes(selection=selection)
+    for n in nodes:
+        n.imageSearchPath.set(str(path))
+
+def get_input_cache(node):
+    return node.cacheFileName.get()
+
+def create_cache(node, _range=(1, 3), samples=3, cache_dir=None):
+    """
+    create cache for yeti node
+    :param node:
+    :return:
+    """
+
+    # create output file name
+    file_name = node.getParent().name()
+    file_name += '.%04d.fur'
+    # Set the output directory if not defined
+    if not cache_dir:
+        cache_dir = get_project_dir(_type='fileCache')
+    file_name = os.path.join(cache_dir, file_name)
+    logger.info('time range: {_range}, samples: {samples}'.format(_range=_range, samples=samples))
+
+    # Check if we are either loaded a groom file or not using a loaded cache
+    groom_file = node.cacheFileName.get()
+    if groom_file:
+        if groom_file.endswith('.grm'):
+            try:
+                node.YetiGroomFile.set(str(groom_file))
+            except AttributeError:
+                node.addAttr("YetiGroomFile", dt="string")
+                node.YetiGroomFile.set(str(groom_file))
+    elif node.fileMode.get() != 0:
+        logger.info('You are already using a cache')
+        return
+
+    # Create cache
+    logger.info('Writing cache: {}'.format(file_name))
+    cmd = 'pgYetiCommand -writeCache "{file_path}" -range {start} {stop} -samples {samples}'
+    pm.mel.eval(cmd.format(file_path=file_name, start=_range[0], stop=_range[1], samples=3))
+
+    # Add notes and original groom file to a custom attribute
+    if groom_file:
+        try:
+            notes = node.notes.get()
+        except AttributeError:
+            node.addAttr('notes', dt="string")
+            notes = '\n\n'
+        notes += 'Original Yeti Cache from {time}:\n' \
+                 '{cache_path}'.format(time=str(datetime.now()), cache_path=groom_file)
+        node.notes.set(notes)
+        logger.info('Added original groom file path to YetiGroomFile attribute')
+
+    # Set file cache on node and change to Cache mode
+    node.cacheFileName.set(file_name)
+    node.fileMode.set(1)
---
+++ python/YetiToolbox.py
@@ -0,0 +1,130 @@
+from maya import OpenMayaUI as omui
+from PySide.QtCore import *
+from PySide.QtGui import *
+from shiboken import wrapInstance
+
+import logging
+logger = logging.getLogger('YetiToolbox')
+
+mayaMainWindowPtr = omui.MQtUtil.mainWindow()
+mayaMainWindow= wrapInstance(long(mayaMainWindowPtr), QWidget)
+
+import YetiHelpers
+reload(YetiHelpers)
+
+
+class LineEdit(QWidget):
+
+    def __init__(self, label, text=None):
+        super(LineEdit, self).__init__()
+        self.layout = QHBoxLayout()
+        self.setLayout(self.layout)
+
+        self.label = QLabel(str(label))
+        self.line_edit = QLineEdit()
+        if text:
+            self.line_edit.setText(str(text))
+
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.line_edit)
+
+    def text(self):
+        return self.line_edit.text()
+
+class ExportDialog(QDialog):
+
+    def __init__(self, time_range, samples, output_path, parent=None):
+        super(ExportDialog, self).__init__(parent)
+        self.setWindowTitle('Create Cache Settings')
+
+        self.time_range = time_range
+        self.samples = samples
+        self.output_path = output_path
+
+        self.layout_edits = QGridLayout()
+        self.main_layout = QVBoxLayout()
+        self.setLayout(self.main_layout)
+
+        self.edit_time_start = LineEdit('Start', time_range[0])
+        self.edit_time_stop = LineEdit('Stop', time_range[1])
+        self.edit_samples = LineEdit('Samples', samples)
+        self.edit_path = LineEdit('Export Path', output_path)
+
+        # OK and Cancel buttons
+        buttons = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel, Qt.Horizontal, self)
+        buttons.accepted.connect(self.send_values)
+        buttons.rejected.connect(self.reject)
+
+        self.layout_edits.addWidget(self.edit_time_start, 0, 0)
+        self.layout_edits.addWidget(self.edit_time_stop, 0, 1)
+        self.layout_edits.addWidget(self.edit_samples, 1, 0)
+
+        self.main_layout.addWidget(self.edit_path)
+        self.main_layout.addLayout(self.layout_edits)
+        self.main_layout.addWidget(buttons)
+
+    def send_values(self):
+        self.time_range = (float(self.edit_time_start.text()), float(self.edit_time_stop.text()))
+        self.samples = int(self.edit_samples.text())
+        self.accept()
+
+class YetiMainWidget(QWidget):
+
+    def __init__(self, parent=None):
+        super(YetiMainWidget, self).__init__(parent)
+        self.setWindowFlags(Qt.Window)
+        self.setProperty("saveWindowPref", True)
+        self.setMinimumWidth(300)
+        self.setWindowTitle('Yeti Toolbox')
+
+        self.main_layout = QGridLayout()
+        self.setLayout(self.main_layout)
+
+        self.__setup_widgets()
+
+    def __setup_widgets(self):
+        self.btn_create_cache = QPushButton('Create Cache')
+        self.btn_create_cache.clicked.connect(self.create_cache)
+
+        self.btn_set_source = QPushButton('Set Image Source Directory')
+        self.btn_set_source.clicked.connect(self.set_image_src)
+
+        self.main_layout.addWidget(self.btn_create_cache, 0, 0)
+        self.main_layout.addWidget(self.btn_set_source, 1, 0)
+
+    def create_cache(self):
+        selection = YetiHelpers.get_selection()
+        nodes = YetiHelpers.get_yeti_nodes(selection)
+        time_range = YetiHelpers.get_time_range()
+
+        # Open setting dialog for export
+        output_path = YetiHelpers.get_project_dir(_type='fileCache')
+        dialog = ExportDialog(time_range, samples=3, output_path=output_path, parent=self)
+
+        if dialog.exec_():
+            time_range = dialog.time_range
+            samples = dialog.samples
+            output_path = dialog.output_path
+            # iterate through nodes and create the caches
+            print time_range, samples, output_path
+            for n in nodes:
+                YetiHelpers.create_cache(n, _range=time_range, samples=samples, cache_dir=output_path)
+
+    def set_image_src(self):
+        default_dir = YetiHelpers.get_project_dir(_type='sourceImages')
+        dialog = QInputDialog(self)
+        dialog.setInputMode(QInputDialog.TextInput)
+        dialog.resize(500, 100)
+        dialog.setWindowTitle('Set Image Source Path')
+        dialog.setTextValue(str(default_dir))
+
+        ok = dialog.exec_()
+        path = dialog.textValue()
+
+        if ok:
+            YetiHelpers.set_image_path(path, selection=False)
+            logger.info('Set path to: {}'.format(path))
+
+def open():
+    yeti_dialog = YetiMainWidget(parent=mayaMainWindow)
+    yeti_dialog.show()
